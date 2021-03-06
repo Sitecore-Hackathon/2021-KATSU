0e034cb4858c78d4b6d61707ade0e8dfbd867516
AJ <jaffal83@outlook.com>
2021-03-06 08:46:00 -0800

    Add New Tenant Script

---
+++ src/Foundation/Scaffolding/website/Texts.cs
@@ -0,0 +1,11 @@
+﻿using Sitecore.Globalization;
+
+namespace Katsu.Foundation.Scaffolding
+{
+    [LocalizationTexts(ModuleName = "Katsu.Foundation.Scaffolding")]
+    public static class Texts
+    {
+        public const string Marketplace = "Marketplace";
+        public const string SiteTemplate = "Site Template";
+    }
+}
